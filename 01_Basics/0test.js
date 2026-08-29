c4bb8eafa38c0ffeaeb8a34ee7aa18af8480519e
Tirupati Bala Jee Mishra <tirupatibalajeemishra@gmail.com>
2026-08-30 01:19:28 +0530

    Datatype in js

---
+++ 01_Basics/0test.js
@@ -0,0 +1,4 @@
+console.log("Tirupati Bala Jee Mishra");
+console.log("Hello JavaScript ! from github repositories");
+console.log("Hello JavaScript ! from github codespaces");
+console.log("Hello JavaScript ! from my local code Editor(VS)");
---
+++ 01_Basics/2Datatypes.js
@@ -0,0 +1,24 @@
+//talking about datatypes in JavaScript...
+
+//all the js code will be treated as newer version or newer js ,,as in previous js there is no conecpt of classes or modulees ,arrow function....
+"use strict";
+
+//alert("hello")  //cannot run till now as we are using nodejs not broowser,so in nodejs there is different syntax to use alert
+console.log(9 + 3);// maintain code readability
+console.log("here ,,we are ");
+
+//datatypes
+let name = "tirupati"; //string datatype
+let age = 21; //number data type
+let isLoggedIn = false; //boolean datatype
+//null => a dattype or we can say that it is a stand alone value, or a representation of empty value,,,we can assign it to variable too
+let accountstate = null;
+console.log(accountstate);
+//symbol=?somethign unique
+//bigint
+//undefined =>when no value is assigned to a variable
+
+console.log(typeof " tirupati");//helps in giving type of the value given to it...
+console.log(typeof age);
+console.log(typeof null);  //the type of null comes out to be=> object  
+console.log(typeof undefined); //undefined
