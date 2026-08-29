dfdc721e62bdab86a6bce8c73d55700073046d17
Tirupati Bala Jee Mishra <160836729+tirupatibalajeemishra@users.noreply.github.com>
2026-08-30 01:20:38 +0530

    Delete 1test.js

--- 01_Basics/1test.js
+++
@@ -1,4 +0,0 @@
-console.log("Tirupati Bala Jee Mishra");
-console.log("Hello JavaScript ! from github repositories");
-console.log("Hello JavaScript ! from github codespaces");
-console.log("Hello JavaScript ! from my local code Editor(VS)");
