e9c1516bead11d43bf54201bd4d1fdb3b211152b
Tirupati Bala Jee Mishra <tirupatibalajeemishra@gmail.com>
2026-08-30 16:08:11 +0530

    Datatype summary

---
+++ 01_Basics/5Datatype_Summary.js
@@ -0,0 +1,67 @@
+//🔴.Datatypes Summary...........📙
+
+// 📙. On the basis of how data is stored in memory and how it can be accessed ,, datatypes are divided into two categories :-
+
+//📙. 1. PRIMITIVE (Call By Value) AND 2. NON-PRIMITIVE(Refehttps://www.youtube.com/watch?v=M0YImBHQsWU&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=29&pp=iAQBrnce type)
+
+//📙.PRIMITIVE :=> 7 categories =>String, Number, Boolean, null, undefined, Symbol(used to make a value unique ), BigInt 
+
+// 📙. Primitive Datatypes=> it is a given as "call by value"..mtlb isme ham jab bhi koi change karte hai to hame memory kaa reference nhi diya jata...hame copy kar le diya jata hai..jo change karte hai to exact original me change nhi hota, copy me change hota hai.
+
+console.log("**************DATATYPES SUMMARY******************");
+
+const score = 100; //number
+const scoreValue = 100.36; //number
+const isLoggedIn = false; //boolean
+const outSideTemp = null; //null -> null means not 0,it is empty.(kuchh mila hi nhi)
+let userEmail; //simply undefined for now  or can manually do it undefined
+// let userEmail= undefined; // manually bhi undefined kar sakte hai
+
+const id = Symbol('1234'); // for making id unique ,use symbol , pass the value in it and it will return a new unique symbol value
+const anotherid = Symbol('1234'); //although values of both ids are same but both are unique as due to symbol
+
+// lets check whether above two are equal or not
+console.log("whether id and anotherid are equal or not");
+console.log(id);
+console.log(anotherid);
+console.log(id == anotherid); // both are not equal
+console.log(id === anotherid);
+
+//BigInt
+const bigNumber = 243433562353267n; // Automatically used bigInt to store this large num or if num is too large then add 'n' after the num...
+
+
+// 📙. NON-PRIMITIVE (Reference type) : Arrays, Functions, Objects
+
+// 📙. Arrays
+const heroes = ["Saktiman", "Naagraj", "Doga"];
+console.log(heroes);
+
+// 📙. Objects => in curly braces
+let myObj = {
+    name1: "suraj",
+    name2: "varnika", //all are objects ,they can be of any datatype
+    age: 42,
+    course: "B.Tech"
+};
+
+// 📙. Functions
+// storing func in a variable
+
+const myfunc = function () { // this function is stored in "myfunc" variable
+    console.log("Hello World!");
+}
+
+
+// 📙. How to find datatype of anything
+console.log(typeof bigNumber); // give bigint
+console.log(typeof outSideTemp); //=> object
+
+console.log(typeof myfunc); // =>function but called as object function 
+console.log(typeof anotherid) //symbol
+console.log(typeof heroes) //object=> datatype of array and object is called as object, and of function is give as function but really called as object function..
+
+
+myfunc(); // function call=> all things in function execute here
+
+
